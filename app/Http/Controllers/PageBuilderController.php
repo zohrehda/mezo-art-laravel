60374bfb0d3acee7759c299dd43e756c69e8c5c2
zohreh <daeian.zohreh@gmail.com>
2024-04-04 12:32:39 +0330

    wip

---
+++ app/Http/Controllers/PageBuilderController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PageBuilder;
+use Illuminate\Http\Request;
+
+class PageBuilderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $pages_builder = PageBuilder::filter()->get();
+        return $this->retrieve($pages_builder);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = $request->apiValidate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $pages_builder = PageBuilder::create($validator->validated());
+        return $this->createdResponse($pages_builder);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PageBuilder $pageBuilder)
+    {
+        return $this->retrieve($pageBuilder);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, PageBuilder $pageBuilder)
+    {
+        $validator = $request->apiValidate([
+            'title' => 'sometimes',
+            'content' => 'sometimes'
+        ]);
+
+        $pages_builder = $pageBuilder->update($validator->validated());
+        return $this->updatedResponse($pages_builder);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PageBuilder $pageBuilder)
+    {
+        $pageBuilder->delete();
+        return $this->deletedResponse();
+    }
+}
