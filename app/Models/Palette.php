e093ee625be5d7a6c0a4d72ab4bafc97a266a25c
zohrehda <daeian.zohreh@gmail.com>
2024-04-28 07:40:22 +0330

    wip

--- app/Models/Palette.php
+++ app/Models/Palette.php
@@ -3,11 +3,19 @@
 namespace App\Models;
 
 use App\Models\Traits\Filterable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Palette extends Model
 {
-    use HasFactory,Filterable;
+    use HasFactory, Filterable;
     public $table = 'palette';
+
+    public function scopeModelFilter(Builder $query)
+    {
+        $search = request()->input('search');
+        if ($search)
+            $query->where('name', 'like', "%$search%");
+    }
 }
