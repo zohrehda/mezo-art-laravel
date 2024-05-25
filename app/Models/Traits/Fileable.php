1e00ae20ad1e0012d87b54a437006d2cb550156e
zohrehda <daeian.zohreh@gmail.com>
2024-05-25 23:32:30 +0330

    wip

--- app/Models/Traits/Fileable.php
+++ app/Models/Traits/Fileable.php
@@ -8,7 +8,9 @@ trait Fileable
 {
     public function files()
     {
-        return $this->morphToMany(File::class, 'fileable', 'fileables')->withPivot(['section']);
-    //    return $this->morphMany(File::class, 'fileable');
+        return $this->morphToMany(File::class, 'fileable', 'fileables')->withPivot(['section'])
+            ->select('files.*', 'fileables.section as section');
+        ;
+        //    return $this->morphMany(File::class, 'fileable');
     }
 }
